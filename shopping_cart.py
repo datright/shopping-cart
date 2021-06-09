adc72e6ec9c46e776cd204106acb3e460fce33d5
datright <84527629+datright@users.noreply.github.com>
2021-06-09 16:58:30 -0400

    Calculating Total Price and Printing

--- shopping_cart.py
+++ shopping_cart.py
@@ -34,7 +34,9 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-# Capture and validate user selections
+# 1. Capture and validate user selections
+total_price = 0
+
 while True:
     selected_id = input("Please input a product identifier:") #> string
     if selected_id == "DONE":
@@ -44,13 +46,15 @@ while True:
 #print(type(selected_id))
         matching_products = [p for p in products if p["id"] == float(selected_id)]
         matching_product = matching_products[0]
-
+        total_price = total_price + matching_product["price"]
         print("Selected Product:", matching_product["name"],"",str(matching_product["price"]))
 
 #print(matching_product)
 #print(type(matching_product))
 
-# Info Display 
+# 2. Info Display 
+
+print("Total Price:", total_price)
 
 #print(products)
 
